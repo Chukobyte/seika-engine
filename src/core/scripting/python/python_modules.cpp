78ef1d12edbc87767de03c130f69ce9700c6ff46
chukobyte <chukobyte@gmail.com>
2021-05-08 10:31:10 -0400

    Solved problem with passing python instance to script.

--- src/core/scripting/python/python_modules.cpp
+++ src/core/scripting/python/python_modules.cpp
@@ -210,7 +210,9 @@ PyObject* PythonModules::scene_tree_get_current_scene_node(PyObject *self, PyObj
     Logger::GetInstance()->Debug("Current scene entity = " + std::to_string(sceneContext->currentSceneEntity));
     if (pythonCache->HasActiveInstance(sceneContext->currentSceneEntity)) {
         Logger::GetInstance()->Debug("Entity found!");
-        return pythonCache->GetClassInstance(sceneContext->currentSceneEntity);
+        CPyObject &pClassInstance = pythonCache->GetClassInstance(sceneContext->currentSceneEntity);
+        pClassInstance.AddRef();
+        return pClassInstance;
     }
     // Creates new instance on script side if active script instance of entity doesn't exist
     return Py_BuildValue("(si)", "Node2D", sceneContext->currentSceneEntity);
