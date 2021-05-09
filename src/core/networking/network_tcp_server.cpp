fedb7b9c016f56f068242608633b69986b924b3c
chukobyte <chukobyte@gmail.com>
2021-05-09 17:00:12 -0400

    Made small progress.

--- src/core/networking/network_tcp_server.cpp
+++ src/core/networking/network_tcp_server.cpp
@@ -7,27 +7,13 @@ NetworkTCPServer::NetworkTCPServer(asio::io_context &context, int port) : contex
 }
 
 void NetworkTCPServer::Start() {
-    logger->Debug("Starting server accepting connections!");
-
-    asio::ip::tcp::socket socket(context);
-//    acceptor.accept(socket);
-
-    auto acceptHandler = [](const asio::error_code& errorCode) {
-        if (!errorCode) {
-            std::cout << "New connection established" << std::endl;
-        }
-    };
-
-//    acceptor.accept(socket);
-    acceptor.async_accept(socket, acceptHandler);
-
     logger->Debug("Server accepting connections!");
 
     std::thread threadContext = std::thread([&]() {
         context.run();
     });
 
-//    context.run();
+    AcceptConnections();
 
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(20000ms);
@@ -36,5 +22,24 @@ void NetworkTCPServer::Start() {
     context.stop();
     if (threadContext.joinable()) {
         threadContext.join();
+    }
+}
+
+void NetworkTCPServer::AcceptConnections() {
+    TCPConnection *tcpConnection = TCPConnection::Create(context);
+
+    auto acceptHandler = [](const asio::error_code& errorCode) {
+        if (!errorCode) {
+//            tcpConnection->Start();
+            std::cout << "New connection established" << std::endl;
+        }
+    };
+
+    acceptor.async_accept(tcpConnection->GetSocket(), acceptHandler);
+}
+
+void NetworkTCPServer::HandleAccept(const asio::error_code &errorCode) {
+    if (!errorCode) {
+        std::cout << "New connection established" << std::endl;
     }
 }
--- src/core/networking/network_tcp_server.h
+++ src/core/networking/network_tcp_server.h
@@ -7,8 +7,24 @@
 #include "../utils/logger.h"
 
 class TCPConnection {
+  private:
+    asio::ip::tcp::socket socket;
   public:
+    TCPConnection(asio::io_context &context) : socket(context) {}
 
+    asio::ip::tcp::socket& GetSocket() {
+        return socket;
+    }
+
+    void HandleWrite(const asio::error_code& errorCode) {
+
+    }
+
+    static TCPConnection* Create(asio::io_context &context) {
+        return new TCPConnection(context);
+    }
+
+    void Start() {}
 };
 
 class NetworkTCPServer {
@@ -25,7 +41,13 @@ class NetworkTCPServer {
     Logger *logger = nullptr;
   public:
     NetworkTCPServer(asio::io_context &context, int port);
+
     void Start();
+
+    void AcceptConnections();
+
+    void HandleAccept(const asio::error_code& errorCode);
+
 };
 
 #endif //NETWORK_TCP_SERVER_H
