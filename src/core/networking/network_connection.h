8f786e3968fea8ad79851b0cbe6ddbdf06686dfc
chukobyte <chukobyte@gmail.com>
2021-05-09 23:32:09 -0400

    Cleaned up connection.

--- src/core/networking/network_connection.h
+++ src/core/networking/network_connection.h
@@ -6,40 +6,28 @@
 using NetworkConnectionId = int;
 
 class NetworkConnection {
-//  protected:
-//    Logger *logger = nullptr;
-//public:
-//    asio::ip::tcp::socket socket;
-//    NetworkConnection(asio::io_context &context) : socket(context), logger(Logger::GetInstance()) {}
-//
-//    virtual asio::ip::tcp::socket& GetSocket() = 0;
-//
-//    virtual void SendNetworkMessage(const std::string &message) = 0;
-//
-//    virtual void ReadNetworkMessages() = 0;
-//
-//    virtual void Start() = 0;
-//
-//    virtual bool IsConnected() = 0;
-};
-
-class TCPConnection : public NetworkConnection {
-  private:
-    asio::ip::tcp::socket socket;
-    std::vector<char> networkBuffer;
-    const unsigned int NETWORK_BUFFER_SIZE = 20 * 1024;
+  protected:
     Logger *logger = nullptr;
+    asio::ip::tcp::socket socket;
   public:
-//    TCPConnection(asio::io_context &context) : NetworkConnection(context) {
-//        networkBuffer.resize(NETWORK_BUFFER_SIZE);
-//    }
+    NetworkConnection(asio::io_context &context) : socket(context), logger(Logger::GetInstance()) {}
 
-    TCPConnection(asio::io_context &context) : socket(context), logger(Logger::GetInstance()) {
-        networkBuffer.resize(NETWORK_BUFFER_SIZE);
+    bool IsConnected() {
+        return socket.is_open();
     }
 
     asio::ip::tcp::socket& GetSocket() {
         return socket;
+    }
+};
+
+class TCPConnection : public NetworkConnection {
+  private:
+    std::vector<char> networkBuffer;
+    const unsigned int NETWORK_BUFFER_SIZE = 20 * 1024;
+  public:
+    TCPConnection(asio::io_context &context) : NetworkConnection(context) {
+        networkBuffer.resize(NETWORK_BUFFER_SIZE);
     }
 
     void SendNetworkMessage(const std::string &message)  {
@@ -70,10 +58,6 @@ class TCPConnection : public NetworkConnection {
     void Start() {
         SendNetworkMessage("[FROM SERVER] Hello from server!\n");
         ReadNetworkMessages();
-    }
-
-    bool IsConnected() {
-        return socket.is_open();
     }
 };
 
