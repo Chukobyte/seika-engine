a727450047a06d7ed89bffd07043c66605f0053e
chukobyte <chukobyte@gmail.com>
2021-05-12 17:07:59 -0400

    More testing.

---
+++ assets/game_projects/fighter/src/input_buffer.py
@@ -0,0 +1,34 @@
+from roll.input import Input
+
+class InputBuffer:
+    """
+    Responsible for collecting game input from both players.  The game state will pull data from here if needed.
+    Network messages will also update the input buffer when receiving data from the opposite player
+    """
+
+    class Value:
+        LEFT = "l"
+        RIGHT = "r"
+        UP = "u"
+        DOWN = "d"
+
+    def __init__(self):
+        self._inputs = []
+
+    def add_input(self, input: str) -> None:
+        self._inputs.append(input)
+
+    def get_inputs(self) -> list:
+        return self._inputs
+
+    def is_empty(self) -> bool:
+        return len(self._inputs) == 0
+
+    def clear(self):
+        self._inputs.clear()
+
+    def poll_client_inputs(self) -> None:
+        if Input.is_action_pressed(action_name="left"):
+            self.add_input(input=InputBuffer.Value.LEFT)
+        elif Input.is_action_pressed(action_name="right"):
+            self.add_input(input=InputBuffer.Value.RIGHT)
--- assets/game_projects/fighter/src/main.py
+++ assets/game_projects/fighter/src/main.py
@@ -3,21 +3,35 @@ from roll.input import Input
 from roll.engine import Engine
 from roll.math import Vector2
 
+from assets.game_projects.fighter.src.input_buffer import InputBuffer
+
+# TODO: Reduce game to 60 frames per second instead of 100
 
 class Main(Node2D):
     def _start(self) -> None:
+        self.player_one_input_buffer = InputBuffer()
         self.player_one_node = self.get_node(name="PlayerOne")
 
     def _physics_process(self, delta_time: float) -> None:
+        # INPUTS
+
         if Input.is_action_just_pressed(action_name="quit"):
             Engine.exit()
-        if Input.is_action_pressed(action_name="left"):
-            self.player_one_node.add_to_position(Vector2.LEFT())
-            self.player_one_node.play(animation_name="Walk")
-            self.player_one_node.flip_h = True
-        elif Input.is_action_pressed(action_name="right"):
-            self.player_one_node.add_to_position(Vector2.RIGHT())
-            self.player_one_node.play(animation_name="Walk")
-            self.player_one_node.flip_h = False
-        else:
+
+        self.player_one_input_buffer.clear()
+        self.player_one_input_buffer.poll_client_inputs()
+
+        # SIMULATION
+
+        if self.player_one_input_buffer.is_empty():
             self.player_one_node.play(animation_name="Idle")
+        else:
+            for input in self.player_one_input_buffer.get_inputs():
+                if input == InputBuffer.Value.LEFT:
+                    self.player_one_node.add_to_position(Vector2.LEFT())
+                    self.player_one_node.play(animation_name="Walk")
+                    self.player_one_node.flip_h = True
+                elif input == InputBuffer.Value.RIGHT:
+                    self.player_one_node.add_to_position(Vector2.RIGHT())
+                    self.player_one_node.play(animation_name="Walk")
+                    self.player_one_node.flip_h = False
