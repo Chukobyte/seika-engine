620763f46c8c695d9777033abf2b2b7f7229e2ac
chukobyte <chukobyte@gmail.com>
2021-05-11 23:27:43 -0400

    Testing animation successfully with old fighter sprite.

--- assets/game_projects/fighter/src/main.py
+++ assets/game_projects/fighter/src/main.py
@@ -1,6 +1,10 @@
 from roll.node import AnimatedSprite
+from roll.input import Input
 
 
 class Main(AnimatedSprite):
-    def _start(self) -> None:
-        print("In Main")
+    def _physics_process(self, delta_time: float) -> None:
+        if Input.is_action_pressed(action_name="left"):
+            self.stop()
+        elif Input.is_action_pressed(action_name="right"):
+            self.play(animation_name="Idle")
