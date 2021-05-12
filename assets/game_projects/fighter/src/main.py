395ce7fbf2790cf773581a02355163b3b8d39089
chukobyte <chukobyte@gmail.com>
2021-05-12 16:11:00 -0400

    Updating main scene.

--- assets/game_projects/fighter/src/main.py
+++ assets/game_projects/fighter/src/main.py
@@ -1,20 +1,23 @@
-from roll.node import AnimatedSprite
+from roll.node import Node2D, AnimatedSprite
 from roll.input import Input
 from roll.engine import Engine
 from roll.math import Vector2
 
 
-class Main(AnimatedSprite):
+class Main(Node2D):
+    def _start(self) -> None:
+        self.player_one_node = self.get_node(name="PlayerOne")
+
     def _physics_process(self, delta_time: float) -> None:
         if Input.is_action_just_pressed(action_name="quit"):
             Engine.exit()
         if Input.is_action_pressed(action_name="left"):
-            self.add_to_position(Vector2.LEFT())
-            self.play(animation_name="Walk")
-            self.flip_h = True
+            self.player_one_node.add_to_position(Vector2.LEFT())
+            self.player_one_node.play(animation_name="Walk")
+            self.player_one_node.flip_h = True
         elif Input.is_action_pressed(action_name="right"):
-            self.add_to_position(Vector2.RIGHT())
-            self.play(animation_name="Walk")
-            self.flip_h = False
+            self.player_one_node.add_to_position(Vector2.RIGHT())
+            self.player_one_node.play(animation_name="Walk")
+            self.player_one_node.flip_h = False
         else:
-            self.play(animation_name="Idle")
+            self.player_one_node.play(animation_name="Idle")
