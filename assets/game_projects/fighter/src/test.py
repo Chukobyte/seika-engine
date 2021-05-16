506f09dfd3a6feef17b0f376ce064e0eb1e815d2
chukobyte <chukobyte@gmail.com>
2021-05-16 16:06:01 -0400

    Added method to extract valid inheritance class name.

--- assets/game_projects/fighter/src/test.py
+++ assets/game_projects/fighter/src/test.py
@@ -5,17 +5,31 @@ from roll.node import CollisionShape2D
 
 class Test(CollisionShape2D):
     @classmethod
+    def extract_valid_inheritance_node(cls) -> str:
+        inheritance_list = [
+            "Node",
+            "Node2D",
+            "Sprite",
+            "AnimatedSprite",
+            "TextLabel",
+            "CollisionShape2D",
+        ]
+        for class_path in cls.__mro__:
+            if class_path.__name__ in inheritance_list:
+                return class_path.__name__
+        return ""
+
+    @classmethod
     def new(cls):
         return roll_engine_api.node_new(
-            class_path="assets.game_projects.fighter.src.test",
-            class_name="Test",
-            node_type="CollisionShape2D",
+            class_path=f"{__name__}",
+            class_name=f"{__class__.__name__}",
+            node_type=f"{cls.extract_valid_inheritance_node()}",
         )
 
     def _start(self) -> None:
-        # print(f"Start called! Rect = {self.collider_rect}")
-        # self.collider_rect = Rect2(x=300, y=300, w=40, h=40)
-        pass
+        self.collider_rect = Rect2(x=300, y=300, w=40, h=40)
+        print(f"Start called! Rect = {self.collider_rect}")
 
     def _physics_process(self, delta_time: float) -> None:
         # print("Process!")
