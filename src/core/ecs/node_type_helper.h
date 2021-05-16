8197b21961514e93d6a48b778d7ecf0a9746e364
chukobyte <chukobyte@gmail.com>
2021-05-16 15:40:10 -0400

    Clean up.

--- src/core/ecs/node_type_helper.h
+++ src/core/ecs/node_type_helper.h
@@ -20,38 +20,15 @@ class NodeTypeHelper {
     static std::map<NodeType, NodeTypeInheritance> NODE_TYPE_TO_INHERITANCE_MAP;
   public:
     static std::string GetNodeTypeString(NodeType nodeType) {
-        switch (nodeType) {
-        case NodeType_NODE:
-            return NODE_TYPE_NODE;
-        case NodeType_NODE2D:
-            return NODE_TYPE_NODE2D;
-        case NodeType_SPRITE:
-            return NODE_TYPE_SPRITE;
-        case NodeType_ANIMATED_SPRITE:
-            return NODE_TYPE_ANIMATED_SPRITE;
-        case NodeType_TEXT_LABEL:
-            return NODE_TYPE_TEXT_LABEL;
-        case NodeType_COLLISION_SHAPE2D:
-            return NODE_TYPE_COLLISION_SHAPE2D;
-        default:
-            return NODE_TYPE_INVALID;
+        if (NODE_TYPE_TO_STRING_MAP.count(nodeType) > 0) {
+            return NODE_TYPE_TO_STRING_MAP[nodeType];
         }
+        return NODE_TYPE_INVALID;
     }
     static NodeType GetNodeTypeInt(const std::string &nodeTypeString) {
-        if (nodeTypeString == NODE_TYPE_NODE) {
-            return  NodeType_NODE;
-        } else if(nodeTypeString == NODE_TYPE_NODE2D) {
-            return NodeType_NODE2D;
-        } else if(nodeTypeString == NODE_TYPE_SPRITE) {
-            return NodeType_SPRITE;
-        } else if(nodeTypeString == NODE_TYPE_ANIMATED_SPRITE) {
-            return NodeType_ANIMATED_SPRITE;
-        } else if(nodeTypeString == NODE_TYPE_TEXT_LABEL) {
-            return NodeType_TEXT_LABEL;
-        } else if(nodeTypeString == NODE_TYPE_COLLISION_SHAPE2D) {
-            return NodeType_COLLISION_SHAPE2D;
+        if (NODE_STRING_TO_TYPE_MAP.count(nodeTypeString) > 0) {
+            return NODE_STRING_TO_TYPE_MAP[nodeTypeString];
         }
-
         return NodeType_INVALID;
     }
 
