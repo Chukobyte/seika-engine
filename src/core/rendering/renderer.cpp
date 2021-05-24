24b6a5b541bd71d859678441e7d463b555d8e115
chukobyte <chukobyte@gmail.com>
2021-05-24 16:54:05 -0400

    Have everything except for rotation.

--- src/core/rendering/renderer.cpp
+++ src/core/rendering/renderer.cpp
@@ -46,11 +46,11 @@ void SpriteRenderer::Draw(Texture2D *texture2D, Rect2 *sourceRectangle, Rect2 *d
     model.Set(model.Translation(Vector3(destinationRectangle->x, destinationRectangle->y, 0.0f)));
 //    std::cout << "After First translation\n" << model << std::endl;
 
-//    model.Set(model.Translation(Vector3(0.5f * destinationRectangle->w, 0.5f * destinationRectangle->h, 0.0f)));
+    model.Set(model.Translation(Vector3(0.5f * destinationRectangle->w, 0.5f * destinationRectangle->h, 0.0f)));
 //    std::cout << "After Second translation\n" << model << std::endl;
 //    model.Set(model.Rotate(Vector3(0.0f, 0.0f, 1.0f), rotation));
 //    std::cout << "After rotation\n" << model << std::endl;
-//    model.Set(model.Translation(Vector3(-0.5f * destinationRectangle->w, -0.5f * destinationRectangle->h, 0.0f)));
+    model.Set(model.Translation(Vector3(-0.5f * destinationRectangle->w, -0.5f * destinationRectangle->h, 0.0f)));
 //    std::cout << "After Third translation\n" << model << std::endl;
 
     model.Set(model.Scale(Vector3(destinationRectangle->w, destinationRectangle->h, 1.0f)));
