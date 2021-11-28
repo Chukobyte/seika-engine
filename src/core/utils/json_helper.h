d988e479fbbf699a7a0158425391daf99def726e
chukobyte <chukobyte@gmail.com>
2021-11-27 21:08:40 -0500

    Adding json helper.

---
+++ src/core/utils/json_helper.h
@@ -0,0 +1,40 @@
+#ifndef JSON_HELPER_H
+#define JSON_HELPER_H
+
+#include <iostream>
+#include <cassert>
+
+#include <json/json.hpp>
+
+class JsonHelper {
+  public:
+    static bool HasKey(const nlohmann::json &json, const std::string &key) {
+        try {
+            json.at(key);
+            return true;
+        } catch (nlohmann::json::type_error &te) {
+        } catch (nlohmann::json::out_of_range &oor) {
+        }
+        return false;
+    }
+
+    template<typename T>
+    static T Get(const nlohmann::json &json, const std::string &key) {
+        if (HasKey(json, key)) {
+            return json.at(key);
+        }
+        std::cerr << "Key '" << key << "' doesn't exist!" << std::endl;
+        assert(false && "Key doesn't exist in json!");
+        return T();
+    }
+
+    template<typename T>
+    static T GetDefault(const nlohmann::json &json, const std::string &key, T defaultValue) {
+        if (HasKey(json, key)) {
+            return json.at(key);
+        }
+        return defaultValue;
+    }
+};
+
+#endif //JSON_HELPER_H
